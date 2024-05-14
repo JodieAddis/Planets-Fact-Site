ec38250522e1ed50e74ffddc3f7ec352b27e994d
JodieAddis <jodie.addis.92@gmail.com>
2024-05-14 13:47:48 +0200

    Correction design navbar on Mobile + adding meta description + adding favicon.

--- src/icons/IconMenu/index.tsx
+++ src/icons/IconMenu/index.tsx
@@ -1,8 +1,12 @@
-const Icon = () => {
+interface IconMenuProps {
+  color: string;
+}
+
+const Icon = ({ color }: IconMenuProps) => {
   return (
     <>
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="17">
-        <g fill="#FFF" fillRule="evenodd">
+        <g fill={color} fillRule="evenodd">
           <path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z" />
         </g>
       </svg>
